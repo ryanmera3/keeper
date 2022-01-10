910a48cbb2d0707b5dba34e306a743583e4229c9
Ryan Mera <ryanmera@hotmail.com>
2022-01-09 22:08:45 -0700

    Frontend start

--- keeper.client/src/components/Navbar.vue
+++ keeper.client/src/components/Navbar.vue
@@ -1,12 +1,8 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img
-          alt="logo"
-          src="../assets/img/cw-logo.png"
-          height="45"
-        />
+        <h1>KEEPER</h1>
       </div>
     </router-link>
     <button
